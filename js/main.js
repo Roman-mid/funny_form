fef2798a1e84b582ad3d825dbe186ed3b5191b0d
Roman <romanmidianko@gmail.com>
2022-11-16 20:11:29 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+const resalt = confirm('Are you here?', '');
+console.log(resalt);
+// const resalt = alert('Are you here?');
+// console.log(resalt);
+
+// const ansver = +prompt('Do you have 18?', '18');
+// console.log(ansver);
+
+// const ansver = [];
+
+// ansver[0] = prompt('What is your name?' , 'name');
+// ansver[1] = prompt('What is your lastname?');
+// ansver[2] = prompt('What is your age?');
+
+// console.log(typeof(ansver));
