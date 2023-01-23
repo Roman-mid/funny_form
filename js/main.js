546573f91a331392ae65459867122df762bb1f95
Roman-mid <roman.midianko@gmail.com>
2023-01-23 19:10:47 +0000

    first commit

--- js/main.js
+++ js/main.js
@@ -1,16 +1,95 @@
 
-const resalt = confirm('Are you here?', '');
-console.log(resalt);
-// const resalt = alert('Are you here?');
-// console.log(resalt);
+"use strict"
 
-// const ansver = +prompt('Do you have 18?', '18');
-// console.log(ansver);
+const body = document.querySelector('body')
+const form = document.querySelector('.form');
+const inputs = form.querySelectorAll('input');
+const userName = form.querySelector('.name');
+const email = form.querySelector('.email');
+const phone = form.querySelector('.phone');
+const btn = form.querySelector('.btn');
 
-// const ansver = [];
+// Validate email
+function getEmail(email) {
+  let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase())
+};
 
-// ansver[0] = prompt('What is your name?' , 'name');
-// ansver[1] = prompt('What is your lastname?');
-// ansver[2] = prompt('What is your age?');
+// Validate phone
+function getPhone(phone) {
 
-// console.log(typeof(ansver));
+  let re = /^\d+$/;
+  return re.test(String(phone));
+}
+
+// Validate phone
+function getName(name) {
+  let re = /^[a-zA-Z]*$/;
+  return re.test(String(name));
+} 
+
+
+function getClass(input) {
+  input.classList.remove('good')
+  input.classList.add('error')
+  body.classList.add('body_style')
+}
+
+
+
+btn.addEventListener('mouseover' , () => {
+
+  let emailValue = email.value;
+  let phoneValue = phone.value.trim();
+  let nameValue = userName.value
+
+
+  inputs.forEach( inp => {
+
+    // Check empty inputs
+    if(inp.value == '') {
+      btn.classList.add('right')
+      getClass(inp)
+    } else {
+      inp.classList.remove('error')
+      inp.classList.add('good')
+      body.classList.remove('body_style')
+    }
+
+    // Check validate email
+    if(!getEmail(emailValue)) {
+      getClass(email)
+    }
+
+    // Check validate phone
+    if(!getPhone(phoneValue) || phoneValue.length < 10) {
+      getClass(phone)
+    }
+
+    
+    // Check validate name
+    if(!getName(nameValue)) {
+      getClass(userName)
+    }
+
+  })
+    
+    if(nameValue && emailValue && phoneValue.length >= 10 && getPhone(phoneValue) && getEmail(emailValue) && getName(nameValue)) {
+      btn.classList.remove('left')
+      btn.classList.remove('right')
+      body.classList.remove('body_style')
+    } else {
+      btn.classList.add('right')
+      body.classList.add('body_style')
+    }
+    
+  // getVecotr(btn)
+  if(btn.classList.contains('left') ) {
+    btn.classList.remove('left')
+    btn.classList.add('right')
+  } else if (btn.classList.contains('right') ) {
+    btn.classList.remove('right')
+    btn.classList.add('left') 
+  }
+
+})
